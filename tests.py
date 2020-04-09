25f9882f902b8109ecb930cb9a62e0f75fa141c6
Arjun Ashok <arjun2000ashok@gmail.com>
2020-04-09 16:25:09 +0530

    package v1

--- tests.py
+++
@@ -1,6 +0,0 @@
-files = [{'index': 0},{'index': 1},{'index': 2}]
-
-for f in files[:]:
-    print(f)
-    files.remove(f)
-print(files)
--- udpServer.py
+++
@@ -1,6 +0,0 @@
-import socket
-
-s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-s.bind(('',1876))
-response = s.recv(1024)
-print(response)
